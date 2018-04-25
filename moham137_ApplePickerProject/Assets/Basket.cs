8413885d4058ab4d27c1c3d1da1e121881d03e97
Habib Mohamed <moham137@uwindsor.ca>
2018-04-25 14:53:05 -0400

    Added game files
    This is the apple picker game

---
+++ moham137_ApplePickerProject/Assets/Basket.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Basket : MonoBehaviour {
+    [Header("Set Dynamically")]
+
+    public Text scoreGT;
+
+    void Start()
+    {
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+        scoreGT = scoreGO.GetComponent<Text>();
+        scoreGT.text = "0";
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
+		
+	}
+
+     void OnCollisionEnter(Collision coll) {                               // 2
+        // Find out what hit this basket
+        GameObject collidedWith = coll.gameObject;                          // 3
+        if (collidedWith.tag == "Apple") {                                // 4
+            Destroy(collidedWith);
+        }
+
+        int score = int.Parse(scoreGT.text);
+        score += 100;
+        scoreGT.text = score.ToString();
+
+        if (score > HighScore.score) {
+            HighScore.score = score;
+        }
+    }
+
+    
+
+}
